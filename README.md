786e44a68e10e584211afe27c7523f3d5b952d70
Shubham <shubhampandey0806@gmail.com>
2023-08-13 11:04:10 +0530

    Completed city repository

--- README.md
+++ README.md
@@ -17,4 +17,22 @@
     "dialect": "mysql"
   } 
 }
+
+- Once  you've added your db config as listed above, go to the src folder from your terminal and execute `npx sequelize db:create` and then execute
+
+`npx sequelize :db migrate`
 ```
+
+
+
+##  DB Design
+    - Airplane Table
+    - Flight Table
+    - City  Table
+    - Airport Table
+
+    - A flight belongs to an airplane but one airplane can be used in multiple flights
+    - A city has many airports but one airport belongs  to a city
+    - One airport can have many flights , but a flight belongs to one airport
+
+  
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,25 +1,57 @@
 const { City }  = require('../models/index');
 
 class CityRepository {  
-    async createCity({name}){
+    async createCity({ name }){
       try {
-        const city = await City.create({name});
-        return city;
+          const city= await City.create({
+            name
+          });
+          return city;
       } catch (error) {
-         throw {error};
-      }  
+          console.log("Something went wrong in the repository layer")
+          throw(error);
+      }
+      
     }
 
     async deleteCity(cityId){
-        try {
+      try {
            await City.destroy({
-              where :{
+              where:{
                 id:cityId
               }
            });
-        } catch (error) {
-            throw {error};
-        }
+           return true;
+      } catch (error) {
+        console.log("Something went wrong in the repository layer");
+          throw {error};
+      }
+    }
+
+    async updateCity(cityId,data){
+      try {
+        const city= await City.update(data,{
+          where:{
+            id:cityId
+          }
+        });
+        return city;
+      } catch (error) {
+        console.log("Something went wrong in the repository layer");
+          throw {error};
+      }
+
+    }
+    async getCity(cityId){
+      try {
+        const city=await City.findByPk(cityId);
+        return City;
+      } catch (error) {
+        console.log("Something went wrong in the repository layer");
+          throw {error};
+      }
+
+
     }
 }
 
