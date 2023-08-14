69e4a588b2f4207b2b3cc874e77e2e081f33f3cc
Shubham <shubhampandey0806@gmail.com>
2023-08-14 19:30:11 +0530

    Added airports model

--- src/migrations/20230814113950-create-airport.js
+++ src/migrations/20230814113950-create-airport.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Cities', {
+    await queryInterface.createTable('Airports', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -10,8 +10,21 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
+        type: Sequelize.STRING
+      },
+      address: {
         type: Sequelize.STRING,
-        allowNull:false
+       
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        onDelete:'CASCADE',
+        references:{
+          model:'Cities',
+          key:'id',
+          as:'cityId'
+        }
+         
       },
       createdAt: {
         allowNull: false,
@@ -24,6 +37,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Cities');
+    await queryInterface.dropTable('Airports');
   }
 };
